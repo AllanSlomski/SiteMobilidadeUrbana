43d7c960e3e348e1269eabd476c715496223122c
Allan Slomski de Araujo <allan.swomki@gmail.com>
2024-07-01 11:54:38 -0300

    alteraçoes no js

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-let tipoVeiculo = document.querySelector("#tipoveiculo");
-let kmPorDia = document.querySelector("#kmPorDia");
-const valorCO2 = document.querySelector("#valorCO2");
-const btn = document.querySelector("#CalcularCo2");
+const tipoVeiculo = document.getElementById("tipoVeiculo");
+const kmPorDia = document.getElementById("kmPorDia");
+const valorCO2 = document.getElementById("valorCO2");
+const btn = document.getElementById("CalcularCo2");
 
 btn.addEventListener('click', calcular);
     function calcular() {
-        valorCO2.style.color = 'blue';
+        var co2emitido = 0;
+        switch (tipoVeiculo.value){
+            case 'motos':
+                co2emitido = kmPorDia * 0.6;
+                break;
+            case 'carros': 
+                co2emitido = kmPorDia * 0.9;
+                break;
+            case 'caminhoes':
+                co2emitido = kmPorDia * 1.3;
+                break;
+            default:
+                co2emitido = 0;
+                break;
+        }
+        valorCO2.innerText = co2emitido;
     }
