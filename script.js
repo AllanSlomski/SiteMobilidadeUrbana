5d5b5c92bd44e922c64a8e7f91fd52fe0f512957
Allan Slomski de Araujo <allan.swomki@gmail.com>
2024-07-01 10:05:59 -0300

    Alterações no css
    Estilos corrigidos, responsividade corrigida.

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-var  valorCO2 = document.querySelector("#valorCO2");
+let tipoVeiculo = document.querySelector("#tipoveiculo");
+let kmPorDia = document.querySelector("#kmPorDia");
+const valorCO2 = document.querySelector("#valorCO2");
+const btn = document.querySelector("#CalcularCo2");
 
-document.getElementById('CalcularCo2').addEventListener('click', function() {
-    // Obter os valores dos inputs
-    var tipoVeiculo = document.getElementById('tipoveiculo').value;
-    var kmPorDia = document.getElementById('kmPorDia').value;
-
-    // Fatores de emissão de CO2 por km para cada tipo de veículo
-    var fatoresEmissao = {
-        motos: 0.09,  // 0.09 kg CO2 por km
-        carros: 0.21, // 0.21 kg CO2 por km
-        caminhoes: 0.55 // 0.55 kg CO2 por km
-    };
-
-    // Calcular a emissão de CO2
-    var emissaoCO2 = kmPorDia * fatoresEmissao[tipoVeiculo];
-
-    // Atualizar o texto do elemento <span id="valorCO2">
-    document.getElementById('valorCO2').textContent = emissaoCO2.toFixed(2);
-});
+btn.addEventListener('click', calcular);
+    function calcular() {
+        valorCO2.style.color = 'blue';
+    }
